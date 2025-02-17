8d2f6b18df20a6d9d53c9578df4d03ae99db1d07
Andrew Ettenger <ettenger@gmail.com>
2025-02-16 19:23:19 -0500

    Github pages take 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // If you're deploying to a subdirectory (like GitHub Pages)
+  basePath: "/band-quiz",
+  assetPrefix: "/band-quiz/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
